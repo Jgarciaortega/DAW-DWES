7b56908aee95e705b4562c3fce516b2d5fcec87a
Javier Garcia Ortega <txhavi@gmail.com>
2019-10-01 19:43:18 +0200

    ok

--- 5.2.php
+++ 5.2.php
@@ -1,24 +1,57 @@
 <html>
+<head>
 
+    <style>
+
+        td{
+
+            border: 1px solid black;
+        }
+
+    </style>
+
+</head>
 
     <?php
 
-$num = 1;
+$num1 = 0;
+$num2 = 0;
+$num3 = 0;
 
-while($num < 100){
+echo "<table>";
 
-echo "<tr>";
+while ($num1 < 10) {
 
-    echo "<td>";
-       echo $num;
-    echo "</td>";
+    echo "<tr>";
 
-echo "</tr>";
+    while ($num2 < 11) {
 
-$num++;
+        if ($num1 == 0 && $num2 < 10) {
 
+            echo "<th>";
+            echo "Tabla del " . ($num2 + 1);
+            echo "</th>";
+
+        }
+
+        if ($num1 > 0) {
+
+            echo "<td>";
+            echo $num2 * $num3;
+            echo "</td>";
+        }
+
+        $num2++;
+
+    }
+    $num1++;
+    $num2 =1;
+    $num3++;
+
+    echo "</tr>";
 }
 
+echo "</table";
 
 ?>
 
